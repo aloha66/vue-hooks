57959b6142205fd930285fcd35b9ec5a5761621d
aloha66 <lzcbruce@live.com>
2021-04-27 10:57:45 +0800

    build: 新增单个模块发布
    可存参数模块清空多个模块的dist

--- scripts/build.ts
+++ scripts/build.ts
@@ -3,6 +3,9 @@ import consola from 'consola';
 import { execSync as exec } from 'child_process';
 const rootDir = path.resolve(__dirname, '..');
 
+const args = require('minimist')(process.argv.slice(2));
+const targets = args._;
+
 async function build() {
   consola.info('Clean up');
   exec('yarn run clean', { stdio: 'inherit' });
@@ -11,9 +14,22 @@ async function build() {
   exec('yarn run build:rollup', { stdio: 'inherit' });
 }
 
+async function fuzzyMatchBuild(targetArr: string[]) {
+  consola.info('Clean up:' + targetArr.join(','));
+  const cmd = targetArr.map((name) => `packages/*/${name}/dist`).join(' ');
+  exec(`npx rimraf dist types ${cmd}`, { stdio: 'inherit' });
+
+  consola.info('Rollup' + targetArr.join(','));
+  // exec(`yarn run build:rollup`, { stdio: 'inherit' }); // 不能传参数给rollup进行打包
+}
+
 async function cli() {
   try {
-    await build();
+    if (!targets.length) {
+      await build();
+    } else {
+      // fuzzyMatchBuild(targets);
+    }
   } catch (e) {
     console.error(e);
     process.exit(1);
--- scripts/release.ts
+++ scripts/release.ts
@@ -1,19 +1,52 @@
 import { execSync } from 'child_process';
+import { resolve, join } from 'path';
 import { readJSONSync } from 'fs-extra';
 
-const { version: oldVersion } = readJSONSync('package.json');
+const args = require('minimist')(process.argv.slice(2));
+// const targets = args._;
+const dest = args.d;
 
-execSync('npx bumpp', { stdio: 'inherit' });
+function release() {
+  const { version: oldVersion } = readJSONSync('package.json');
 
-const { version } = readJSONSync('package.json');
+  execSync('npx bumpp', { stdio: 'inherit' });
 
-if (oldVersion === version) {
-  console.log('canceled');
-  process.exit();
+  const { version } = readJSONSync('package.json');
+
+  if (oldVersion === version) {
+    console.log('canceled');
+    process.exit();
+  }
+
+  execSync('npm run update', { stdio: 'inherit' });
+  execSync('git add .', { stdio: 'inherit' });
+
+  execSync(`git commit -m "chore: release v${version}"`, { stdio: 'inherit' });
+  execSync(`git tag -a v${version} -m v${version}`, { stdio: 'inherit' });
 }
 
-execSync('npm run update', { stdio: 'inherit' });
-execSync('git add .', { stdio: 'inherit' });
+function fuzzyMatchRelease() {
+  const packageDir = `packages/hooks/${dest}`;
+  const packagePath = join(packageDir, 'package.json');
+  const formatWindowPath = packagePath.replace(/\\/g, '/'); // window下会出现两个\\ 直接执行npx bumpp xxx会报错
 
-execSync(`git commit -m "chore: release v${version}"`, { stdio: 'inherit' });
-execSync(`git tag -a v${version} -m v${version}`, { stdio: 'inherit' });
+  const { version: oldVersion } = readJSONSync(packagePath);
+  execSync(`npx bumpp ${formatWindowPath}`, { stdio: 'inherit' });
+  const { version } = readJSONSync(packagePath);
+
+  if (oldVersion === version) {
+    console.log('canceled');
+    process.exit();
+  }
+
+  execSync(`git add ${formatWindowPath}`, { stdio: 'inherit' });
+
+  execSync(`git commit -m "chore: release v${version}"`, { stdio: 'inherit' });
+  execSync(`git tag -a v${version} -m v${version}`, { stdio: 'inherit' });
+}
+
+if (!dest) {
+  release();
+} else {
+  fuzzyMatchRelease();
+}
