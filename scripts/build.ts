cd4ff85d4ad30567afa721f4f79b33b4bb7e94f9
aloha66 <lzcbruce@live.com>
2021-12-05 20:54:30 +0800

    build: 新增clean命令

---
+++ scripts/build.ts
@@ -0,0 +1,33 @@
+import path from 'path'
+import consola from 'consola'
+import { execSync as exec } from 'child_process'
+
+const rootDir = path.resolve(__dirname, '..')
+
+const args = require('minimist')(process.argv.slice(2))
+const targets = args._
+
+async function build() {
+  consola.info('Clean up')
+  exec('pnpm run clean', { stdio: 'inherit' })
+
+  consola.info('Rollup')
+  exec('pnpm run build:rollup', { stdio: 'inherit' })
+}
+
+async function cli() {
+  try {
+    if (!targets.length) {
+      await build()
+    } else {
+      // fuzzyMatchBuild(targets);
+    }
+  } catch (e) {
+    console.error(e)
+    process.exit(1)
+  }
+}
+
+export { build }
+
+if (require.main === module) cli()
